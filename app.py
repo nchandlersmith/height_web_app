c3a6bf6e57e39ca1143d93af801409bcff9fbf2a
Nate Chandler-Smith <nate.chandlersmith@gmail.com>
2018-11-17 16:44:26 -0600

    added database calls

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:POSipa3145@localhost/height_collector'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://flask:flaskpass1234@localhost/height_collector'
 db = SQLAlchemy(app)
 
 class Data(db.Model):
-    __tablename__ = 'data'
+    __tablename__ = 'flask_data'
     id = db.Column(db.Integer, primary_key=True)
     email_ = db.Column(db.String(120), unique=True)
     height_ = db.Column(db.Integer)
@@ -26,7 +26,13 @@ def success():
         email = request.form["email_name"]
         height = request.form["height_name"]
         print("Email: ", email, "\nHeight: ", height)
-    return render_template("success.html")
+        if db.session.query(Data).filter(Data.email_ == email).count() == 0:
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+        return render_template("index.html",
+                               text='We already have something from that email address!')
 
 if __name__ == '__main__':
     app.debug = True
